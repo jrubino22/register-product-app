5e7e09240989fc3d4febab6ab75b3b14b10ef12f
Joseph Rubino <joeyrubi22@gmail.com>
2022-06-16 16:58:06 -0500

    ngrok config

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('isomorphic-fetch')
+const dotenv = require('dotenv')
 const Koa = require('koa')
 const next = require('next')
 const {default: createShopifyAuth} = require('@shopify/koa-shopify-auth');
 const {verifyRequest} = require('@shopify/koa-shopify-auth');
 const { default: Shopify, ApiVersion } = require('@shopify/shopify-api');
-const { Router } = require('express');
+const Router = require('koa-router');
 
 dotenv.config();
 
@@ -24,10 +25,27 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev })
 const handle = app.getRequestHandler();
 
+const ACTIVE_SHOPIFY_SHOPS = {};
+
 app.prepare().then(() => {
     const server = new Koa();
     const router = new Router();
     server.keys = [Shopify.Context.API_SECRET_KEY];
+
+    server.use(
+        createShopifyAuth({
+            afterAuth(ctx) {
+                const {shop, scope} = ctx.state.shopify;
+                ACTIVE_SHOPIFY_SHOPS[shop] = scope;
+
+                if( ACTIVE_SHOPIFY_SHOPS[shop]) {
+                    ctx.redirect(`https://${shop}/admin/apps`)
+                }else{
+                    ctx.redirect(`/?shop=${shop}`);
+                }             
+            },
+        }),
+    );
 
     const handleRequest = async (ctx) => {
         await handle(ctx.req, ctx.res);
@@ -35,8 +53,21 @@ app.prepare().then(() => {
         ctx.res.statusCode = 200;
     };
 
+    router.get("/", async (ctx) => {
+        const shop = ctx.query.shop;
+
+        await handleRequest(ctx);
+    })
+
     router.get('(.*)', handleRequest);
+
+    router.get("(/_next/static/.*)", handleRequest);
+    router.get("/_next/webpack-hmr", handleRequest)
 
     server.use(router.allowedMethods());
     server.use(router.routes());
+
+    server.listen(port, () => {
+        console.log(`Ready on http://localhost:${port}`)
+    })
 })
